eb56765fcea5e2cf937f95abd2342a99d782c436
hondasila <56035181+hondasila@users.noreply.github.com>
2020-11-05 18:02:39 +0200

    #1

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html'); 
-});
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-const http = require('http');
-app.get("/", (request, response) => {
-  response.sendStatus(200); 
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://.glitch.me`);
-}, 280000); 
-
-
 
 var shell = require('shelljs');
   
